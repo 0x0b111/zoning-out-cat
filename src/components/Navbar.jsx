3aba4faaae69f296293c1fed94537d04782e8932
Jay2995 <josey02995@gmail.com>
2024-05-10 17:37:28 +0300

    added links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ function Navbar(){
         <nav className=" bg-black py-4">
             <div className="container mx-auto flex items-center justify-between">
                 <ul className= "flex flex-wrap justify-center  space-x-6 sm:space-x-12 text-white w-full sm:w-auto mx-auto">
-                    <li><a href="">Twitter</a></li>
-                    <li><a href="">Telegram</a></li>
+                    <li><a href="https://twitter.com/ZoningOutCatSol">Twitter</a></li>
+                    <li><a href="https://t.me/ZoningOutCat_SOL">Telegram</a></li>
                     <li><a href="">Dexscreener</a></li>
                     <li><a href="">Solscan</a></li>
                 </ul>
